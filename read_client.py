e66bc7e6b3fc9d091b1113838e86c0a6acc7956e
HYORIMLEE <37135345+HYORIMLEE@users.noreply.github.com>
2021-04-28 15:42:30 +0900

    Update read_client.py

--- read_client.py
+++ read_client.py
@@ -22,25 +22,10 @@ if __name__ == "__main__":
 
         # Node objects have methods to read and write node attributes as well as browse or populate address space
         print("Children of root are: ", root.get_children())
-        
-        # get a specific node knowing its node id
-        #var = client.get_node(ua.NodeId(1002, 2))
-        #var = client.get_node("ns=3;i=2002")
-        #print(var)
-        #var.get_data_value() # get value of node as a DataValue object
-        #var.get_value() # get value of node as a python builtin
-        #var.set_value(ua.Variant([23], ua.VariantType.Int64)) #set node value using explicit data type
-        #var.set_value(3.9) # set node value using implicit data type
-
-        # Now getting a variable node using its browse path
-        #myvar = root.get_child(["0:Objects", "2:MyObject", "2:MyVariable"])
-        #obj = root.get_child(["0:Objects", "2:MyObject"])
-        #print("myvar is: ", myvar)
-        #print("myobj is: ", obj)
 
         # Stacked myvar access
-        print("myvar is: ", objects.get_children()[1].get_variables()[0].get_value())
-        print("myvar is: ", objects.get_children()[1].get_variables()[1].get_value())
+        print("myvar1 is: ", objects.get_children()[1].get_variables()[0].get_value())
+        print("myvar2 is: ", objects.get_children()[1].get_variables()[1].get_value())
 
     finally:
         client.disconnect()
